25df4cb631b2e4c6a84dc417f2578946b32135fa
yaoyuan <yao10475@gmail.com>
2024-04-10 14:47:38 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package com.sky.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis连续工厂对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置redis key的序列化器
+        return redisTemplate;
+
+    }
+}
